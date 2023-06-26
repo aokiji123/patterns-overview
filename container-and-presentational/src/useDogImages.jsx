3281fa671560a365f2ffc6493cb9fb3e94e3b5e8
Bohdan Tolmachov <bohdan.tolmachov@atreo.cz>
2023-06-26 21:55:58 +0200

    container/presentational

---
+++ container-and-presentational/src/useDogImages.jsx
@@ -0,0 +1,19 @@
+import {useEffect, useState} from "react";
+
+export const useDogImages = () => {
+  const [dogs, setDogs] = useState([]);
+
+  useEffect(() => {
+    async function fetchDogs() {
+      const res = await fetch(
+        "https://dog.ceo/api/breed/labrador/images/random/6"
+      );
+      const { message } = await res.json();
+      setDogs(message);
+    }
+
+    fetchDogs();
+  }, []);
+
+  return dogs;
+}
